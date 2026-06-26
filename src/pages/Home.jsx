108a48a7b05511c377cb95b53664f4310c510f3b
Kassim <kassimrooney@gmail.com>
2026-06-26 14:23:08 +0300

    update structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import "../css/Home.css";
+
+import Navbar from "../components/layout/Navbar/Navbar";
+import Hero from "../components/home/Hero/Hero";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
